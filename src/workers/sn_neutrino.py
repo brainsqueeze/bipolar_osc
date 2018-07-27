21bd6d9ed50c7eca46c4979e9e0b7b4a9a8bf38d
brainsqueeze <dave.f.hollander@gmail.com>
2018-07-27 11:54:48 -0400

    Improve the integration steps. Add better documentation.

--- src/workers/sn_neutrino.py
+++ src/workers/sn_neutrino.py
@@ -39,9 +39,8 @@ class SNThermodynamics(object):
         self.delta_e = 0.2
 
         self.Spectra = self.spectra()
-
         self.x, self.u_prime, self.nu_e_dist, self.nu_mu_dist = np.array(list(zip(*self.Spectra)), dtype=np.float32)
-        # self.u_prime = np.unique(self.u_prime)
+        self.unique_cosine_prime = np.unique(self.u_prime)
 
     def f_nu_e(self, x):
         """
@@ -50,7 +49,7 @@ class SNThermodynamics(object):
         :return: initial energy spectrum (numpy array)
         """
         return (self.FluxE / self.FluxTot) * np.power(x / self.EeAvg, 2.) * \
-               np.power(1 + np.exp(x / self.TE - self.EtaE), -1)
+            np.power(1 + np.exp(x / self.TE - self.EtaE), -1)
 
     def f_nu_e_bar(self, x):
         """
@@ -67,8 +66,9 @@ class SNThermodynamics(object):
         :param x: energy (+ for nu, - for anti nu)
         :return: initial energy spectrum (numpy array)
         """
+
         return (self.FluxMu / self.FluxTot) * np.power(x / self.EmuAvg, 2.) * \
-               np.power(1 + np.exp(x / self.TMu - self.EtaMu), -1)
+            np.power(1 + np.exp(x / self.TMu - self.EtaMu), -1)
 
     def spectra(self):
         """
@@ -104,6 +104,7 @@ class SNThermodynamics(object):
         :param r: radius
         :return: Background neutrino density (float)
         """
+
         return (4 / 3) * self.mu_0 * ((self.R / r) ** 3)
 
 
@@ -118,6 +119,7 @@ class Neutrino(SNThermodynamics):
         B-vector in the inverted mass hierarchy.
         :return: 3rd and 8th components of B
         """
+
         # mixing angles and CPV phase
         c12 = np.cos(33.2 * np.pi/180)
         c23 = np.cos(40. * np.pi/180)
@@ -160,9 +162,6 @@ class Neutrino(SNThermodynamics):
         if n == 1:
             return np.zeros_like(cosine)
 
-        # if any([n == 1, energy_momentum == 0]):
-        #     return 0 * x_momentum * u_prime
-
         ratio = (1 / (energy_momentum * cosine) - 1 / (x_momentum * u_prime))
         ratio[energy_momentum == 0] = 0
 
@@ -190,36 +189,8 @@ class Neutrino(SNThermodynamics):
         if cosine < np.power(1. - np.power((self.R / (self.RStar + (n - 1) * self.deltaR)), 2), 0.5):
             return 0
 
-        return log_lam - self.deltaR * self.mu(self.RStar + (n-1) * self.deltaR) / b_orthogonal * integral
-
-    def vectorized_make_tables(self, previous_solution, n_step):
-        log_lambda = previous_solution[:, -1]
-        lambda_func = np.exp(log_lambda)
-
-        spectra = lambda_func * self.sine_func(
-            energy_momentum=previous_solution[:, 0],
-            x_momentum=self.x,
-            cosine=previous_solution[:, 1],
-            u_prime=self.u_prime,
-            n=n_step
-        )
-        spectra = spectra[:, None]
-        spectra = (self.nu_e_dist - self.nu_mu_dist) * spectra
-
-        integrand_array = spectra.reshape(spectra.shape[0] // self.N, self.N, spectra.shape[1])
-        # integrand_array = spectra.reshape(spectra.shape[0], spectra.shape[1] // self.N, self.N)
-
-        # # Casts values into a matrix with u_prime = cosine indexing the columns, and the energy indexing the rows
-        # # (ie. integrand_array[0, 19] corresponds to energy -70 MeV and u_prime = cosine = 1
-        # integrand_array = integrand_array[:, 2].reshape(-1, self.N)
-
-        # Performs first integral over cosine by performing the trapezoidal algorithm over each row of the matrix.
-        # cos_int = np.trapz(integrand_array, dx=np.diff(self.u_prime)[0], axis=1)
-        cos_int = np.trapz(integrand_array, dx=np.diff(np.unique(self.u_prime))[0], axis=1)
-
-        assert isinstance(cos_int, np.ndarray)
-        cos_int[np.isnan(cos_int)] = 0
-        return np.trapz(cos_int, dx=0.2, axis=0)
+        radius = self.RStar + (n - 1) * self.deltaR
+        return log_lam - self.deltaR * self.mu(r=radius) / b_orthogonal * integral
 
     def make_tables(self, energy_momentum, cosine, n, log_lambda):
         """
@@ -236,21 +207,19 @@ class Neutrino(SNThermodynamics):
 
         # Sets numpy arrays for the integrand
         spectra = (self.nu_e_dist - self.nu_mu_dist) * lambda_func * self.sine_func(
-            energy_momentum,
-            self.x,
-            cosine,
-            self.u_prime,
-            n
+            energy_momentum=energy_momentum,
+            x_momentum=self.x,
+            cosine=cosine,
+            u_prime=self.u_prime,
+            n=n
         )
-        # integrand_array = np.hstack((self.x[:, None], self.u_prime[:, None], spectra[:, None]))
 
-        # Casts values into a matrix with u_prime = cosine indexing the columns, and the energy indexing the rows
-        # (ie. integrand_array[0, 19] corresponds to energy -70 MeV and u_prime = cosine = 1
-        # integrand_array = integrand_array[:, 2].reshape(-1, self.N)
+        # reshape the spectra with cosine steps across the columns,
+        # this is preparing to integrate separately along each dimension
         integrand_array = spectra.reshape(-1, self.N)
 
         # Performs first integral over cosine by performing the trapezoidal algorithm over each row of the matrix.
-        cos_int = np.trapz(integrand_array, dx=np.diff(self.u_prime)[0], axis=1)
+        cos_int = np.trapz(integrand_array, x=self.unique_cosine_prime, axis=1)
 
         assert isinstance(cos_int, np.ndarray)
         cos_int[np.isnan(cos_int)] = 0
